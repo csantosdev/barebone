9b977d05f91633e8ed7134012c501860ece8abfc
Chris Santos <chris@inceptures.com>
2015-09-14 17:08:55 -0400

    Moved some args around

--- src/views/binding/BBFormDirective.js
+++ src/views/binding/BBFormDirective.js
@@ -4,14 +4,13 @@ Barebone.Views.Binding.BBFormDirective = Barebone.Views.Binding.Directive.extend
 
         var callback = element.attr('bb-form');
 
-        element.on('submit', function() {
+        element.on('submit', function(event) {
 
             if(!view[callback]) {
                 throw new Error('There is no callback method "' + callback+ '" on view cid "' + view.cid + '"');
             }
 
-            view[callback]($(this).serializeArray(), element);
-            return false;
+            view[callback](event, $(this).serializeArray(), element);
         });
     }
 });
